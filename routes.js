34793501896a8f5dc8c06f5792a683a3d97bbd5b
taaufiqhh <taufiqhidayat227@gmail.com>
2023-05-30 20:09:34 +0700

    Update Ke-2

--- routes.js
+++ routes.js
@@ -3,14 +3,14 @@ const routes = [
         method: '*',
         path: '/',
         handler: (request, h) => {
-            return 'Halaman tidak dapat diakses dengan method tersebut - update';
+            return 'Halaman tidak dapat diakses dengan method tersebut - update Ke-2';
         },
     },
     {
         method: 'GET',
         path: '/',
         handler: (request, h) => {
-            return 'Homepage update';
+            return 'Homepage update Ke-2';
         },
     },
 ];
